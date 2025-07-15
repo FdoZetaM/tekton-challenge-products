401c766e5f43387d3f579cb536284696cae4fea3
Fernando Zapata M <fzapatam.dev@gmail.com>
2025-07-14 22:26:51 -0500

    Add domain entity

---
+++ src/Domain/Entities/Product.cs
@@ -0,0 +1,30 @@
+﻿namespace TektonChallengeProducts.Domain.Entities;
+
+using Domain.Enums;
+using Domain.Entities.Base;
+
+public class Product : Entity<Guid>
+{
+    public Product( Status status,
+                    int stock,
+                    string description,
+                    decimal price,
+                    decimal discountPercentage ) : base(Guid.NewGuid())
+    {
+        this.Status = status;
+        this.Stock = stock;
+        this.Description = description;
+        this.Price = price;
+        this.DiscountPercentage = discountPercentage;
+    }
+
+    public Status Status { get; private set; }
+
+    public int Stock { get; private set; }
+
+    public string Description { get; private set; }
+
+    public decimal Price { get; private set; }
+
+    public decimal DiscountPercentage  { get; private set; }
+}
---
+++ src/Domain/Enums/Status.cs
@@ -0,0 +1,7 @@
+namespace TektonChallengeProducts.Domain.Enums;
+
+public enum Status
+{
+    Active = 0,
+    Inactive = 1,
+}
