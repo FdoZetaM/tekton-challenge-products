ad69ddd1e2d074ad4b63a4a35f2c035f8bf151aa
Fernando Zapata M <fzapatam.dev@gmail.com>
2025-07-15 20:34:56 -0500

    Add infrastructure layer to solution

---
+++ src/Infrastructure/DependencyInjection.cs
@@ -0,0 +1,11 @@
+﻿namespace TektonChallengeProducts.Infrastructure;
+
+using Microsoft.Extensions.DependencyInjection;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure( this IServiceCollection services )
+    {
+        return services;
+    }
+}
