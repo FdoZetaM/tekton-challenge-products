de4d27f936e25993d1d96f33b7f54626400f380a
Fernando Zapata M <fzapatam.dev@gmail.com>
2025-07-18 00:22:04 -0500

    Get product status from cache

--- src/Application/UseCases/GetProductById/GetProductByIdQueryHandler.cs
+++ src/Application/UseCases/GetProductById/GetProductByIdQueryHandler.cs
@@ -1,15 +1,20 @@
 namespace TektonChallengeProducts.Application.UseCases.GetProductById;
 
 using MediatR;
+using Application.Resources;
 using Domain.Abstractions.Persistence;
+using Domain.Abstractions.Services;
+using Domain.Enums;
 
 public class GetProductByIdQueryHandler : IRequestHandler<GetProductByIdQuery, ProductResponse?>
 {
     private readonly IProductRepository productRepository;
+    private readonly ICacheService cacheService;
 
-    public GetProductByIdQueryHandler(IProductRepository productRepository)
+    public GetProductByIdQueryHandler(IProductRepository productRepository, ICacheService cacheService)
     {
         this.productRepository = productRepository;
+        this.cacheService = cacheService;
     }
 
     public async Task<ProductResponse?> Handle(GetProductByIdQuery request, CancellationToken cancellationToken)
@@ -18,11 +23,17 @@ public class GetProductByIdQueryHandler : IRequestHandler<GetProductByIdQuery, P
 
         if (product is null) return null;
 
+        var statusDict = await cacheService.GetAsync<Dictionary<Status, string>>(CacheKeys.ProductStatus)
+                      ?? throw new InvalidOperationException(ValidationMessagesResources.StatusDictionaryNotCached);
+
+        string status = statusDict.GetValueOrDefault(product.Status)
+                     ?? throw new InvalidOperationException(string.Format(ValidationMessagesResources.StatusNotFoundInCache, product.Status));
+
         return new ProductResponse
         {
             Id = product.Id,
             Name = product.Name,
-            Status = product.Status.ToString(),
+            Status = status,
             Stock = product.Stock,
             Description = product.Description,
             Price = product.Price,
